94379906f2231bedaa94c739bcbff0e477949966
yasmeen2001234 <83319374+yasmeen2001234@users.noreply.github.com>
2023-07-09 01:50:53 +0400

    modified readme file , simplified the frustum file code

--- README.md
+++ README.md
@@ -6,15 +6,20 @@ https://www.npmjs.com/package/@bonuz/react-three-third-person
 Which was edited and updated from the original library 
 https://github.com/mannynotfound/react-three-third-person
 
-
 After cloning the repo run the following commands:
 
-npm install or npm install --force ( incase you faced dependency error )
+- npm install or npm install --force ( incase you faced dependency error )
 
-npm run dev
+- npm run dev
 
-![image](https://github.com/yasmeen2001234/r3f-Third-person-joysticks/assets/83319374/7381ea04-508e-4a9c-bdb2-a21ab2abbf14)
 
-https://github.com/yasmeen2001234/r3f-Third-person-joysticks/assets/83319374/686d83f4-5b82-4f9c-9224-94347678b701
+
+Frustum culling is a technique used in computer graphics to improve performance by determining which objects in a 3D scene are visible within the camera's view frustum and should be rendered, and which objects are outside the frustum and can be skipped.
+
+This video shows that whenever the grass is inside the player's view, it is rendered, and otherwise, it is removed from the scene.
+
+https://github.com/yasmeen2001234/r3f-Third-person-joysticks/assets/83319374/3d47cbe4-8128-442f-ba02-1322b3dd5c61
+
+
 
 
--- components/Grass/Frustum _culling_grass1.tsx
+++ components/Grass/Frustum _culling_grass1.tsx
@@ -6,8 +6,6 @@ const Grass = React.lazy(() => import('./Grass'))
 interface Props {
   camera: any
 }
-
-
 export default function Frustum_culling_ocean({ camera }: Props) {
   const mesh = useRef<THREE.InstancedMesh>()
   const [visible, setVisible] = React.useState(true)
@@ -16,26 +14,34 @@ export default function Frustum_culling_ocean({ camera }: Props) {
   useFrame((state, delta) => {
     if (!mesh.current) return
 
-    // Check if the instanced mesh is out of view
-    const camera = state.camera
+    //The code calculates a matrix that combines the camera's projection and position.
+    //Then, using that matrix, it sets up a frustum that defines the visible area of the camera's view.
 
+    const camera = state.camera
     const cameraViewProjectionMatrix = new THREE.Matrix4()
     cameraViewProjectionMatrix.multiplyMatrices(camera.projectionMatrix, camera.matrixWorldInverse)
     frustum.setFromProjectionMatrix(cameraViewProjectionMatrix)
+
+    //The instanced mesh serves as a reference for determining the visibility of the grass.
+
     const box = new THREE.Box3().setFromObject(mesh.current)
     const isBoxOutsideFrustum = !frustum.intersectsBox(box)
-
-   
     mesh.current.instanceMatrix.needsUpdate = true
 
-    if (!isBoxOutsideFrustum) 
-    setVisible(false)
-    else setVisible(true)
+    // Check if the instanced mesh is out of view
+    // if the instanced mesh is outside the camera's frustum we set the grass visibilty to false otherwise true
+    if (isBoxOutsideFrustum) {
+      setVisible(false)
+      console.log('Grass is removed')
+    } else {
+      console.log('Grass is added')
+      setVisible(true)
+    }
   })
 
   return (
     <>
-      <Grass position={[-0.5, -0.3, 0]} width={10} instances={3500} visiblity={!visible} />
+      <Grass position={[-0.5, -0.3, 0]} width={10} instances={3500} visiblity={visible} />
       <instancedMesh ref={mesh} scale={[1, 1, 1]} args={[null, null, 1000]} position={[19, 0, 19]}>
         <boxBufferGeometry args={[9, 1, 9]} />
         <meshStandardMaterial attach='material' color={'white'} opacity={0} transparent={true} />
